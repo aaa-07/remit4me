e2cd93becb6562144d89cadee196fab399087721
aaa-07 <155183733+aaa-07@users.noreply.github.com>
2025-09-25 12:58:27 +0530

    Add files via upload

--- components/home/header/index.jsx
+++ components/home/header/index.jsx
@@ -1,25 +1,27 @@
 import classNames from 'classnames';
-import { Inter } from 'next/font/google';
-import { useState, useEffect } from 'react';
+import {Inter} from 'next/font/google';
+import {useState, useEffect} from 'react';
 import styles from './index.module.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({subsets: ['latin']});
 
 const Header = () => {
-  const [show, setShow] = useState(false);
+  const [show, setShow] = useState(true);
   const [lastScrollY, setLastScrollY] = useState(0);
+  const [screenHeight, setScreenHeight] = useState(0);
 
   useEffect(() => {
     const handleScroll = () => {
       const currentScrollY = window.scrollY;
 
-      if (currentScrollY < lastScrollY) {
+      if (currentScrollY === 0 || currentScrollY < lastScrollY) {
         setShow(true);
       } else if (currentScrollY > lastScrollY) {
         setShow(false);
       }
 
       setLastScrollY(currentScrollY);
+      setScreenHeight(window.innerHeight);
     };
 
     window.addEventListener('scroll', handleScroll);
@@ -27,10 +29,19 @@ const Header = () => {
   }, [lastScrollY]);
 
   return (
-    <div className={classNames(inter.className, styles.header, 'flex items-center text-white transition-transform duration-300', show ? 'translate-y-0' : '-translate-y-full')}>
+    <div
+      className={classNames(
+        inter.className,
+        styles.header,
+        'flex items-center text-white transition-transform duration-300',
+        lastScrollY === 0 ? '' : 'backdrop-blur-md',
+        show ? 'translate-y-0' : '-translate-y-full',
+        lastScrollY > 982 && lastScrollY < 6 * screenHeight ? 'bg-[#092929]' : lastScrollY === 0 ? 'bg-transparent' : 'bg-[#ffffff29]'
+      )}
+    >
       <div className='text-[20px]/[20px] font-bold'>remit4me</div>
-      <div className='ml-auto text-[16px]/[16px] font-medium'>About us</div>
-      <div className='text-[16px]/[16px] font-medium mx-6 cursor-pointer' onClick={() => window.scrollTo({ top: 982, behavior: 'smooth' })}>
+      {/* <div className='ml-auto text-[16px]/[16px] font-medium'>About us</div> */}
+      <div className='text-[16px]/[16px] font-medium mx-6 cursor-pointer ml-auto' onClick={() => window.scrollTo({top: 982, behavior: 'smooth'})}>
         Why choose us
       </div>
       <div className='text-[16px]/[16px] font-medium cursor-pointer' onClick={() => (window.location.href = '#waitlist')}>
